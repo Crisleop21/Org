fb6cdf27abff4fbc08165ce82dbba99e3a173ce7
Crisleop21 <leonelporoj@gmail.com>
2023-05-10 01:18:57 -0600

    Creacion de Header y Formulario

---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,28 @@
+import './Formulario.css';
+import CampoTexto from '../CampoTexto';
+import ListaOpciones from '../ListaOpciones';
+import Boton from '../Boton';
+
+const Formulario = () => {
+
+    const manejarEnvio = (event) => {
+        event.preventDefault();
+        console.log('Manejar el Envio', event)
+    }
+
+    return <section className="formulario">
+        <form onSubmit={manejarEnvio} >
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required />
+            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
+            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required />
+            <ListaOpciones />
+            {/* <Boton texto="Crear" /> */}
+            <Boton>
+                Crear
+            </Boton>
+        </form>
+    </section>
+}
+
+export default Formulario;
