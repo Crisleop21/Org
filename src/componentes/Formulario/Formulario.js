ec448ef879236b32f5c507007f848490ca3cab28
Crisleop21 <leonelporoj@gmail.com>
2023-05-29 23:11:57 -0600

    Curso React finalizado

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,28 +1,92 @@
-import './Formulario.css';
-import CampoTexto from '../CampoTexto';
-import ListaOpciones from '../ListaOpciones';
-import Boton from '../Boton';
+import { useState } from "react";
+import "./Formulario.css";
+import Campo from "../Campo";
+import ListaOpciones from "../ListaOpciones";
+import Boton from "../Boton";
 
-const Formulario = () => {
+const Formulario = (props) => {
+  const [nombre, actualizarNombre] = useState("");
+  const [puesto, actualizarPuesto] = useState("");
+  const [foto, actualizarFoto] = useState("");
+  const [equipo, actualizarEquipo] = useState("");
+  
+  const [titulo, actualizarTitulo] = useState("")
+  const[color, actualizarColor] = useState("")
 
-    const manejarEnvio = (event) => {
-        event.preventDefault();
-        console.log('Manejar el Envio', event)
-    }
 
-    return <section className="formulario">
-        <form onSubmit={manejarEnvio} >
-            <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required />
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
-            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required />
-            <ListaOpciones />
-            {/* <Boton texto="Crear" /> */}
-            <Boton>
-                Crear
-            </Boton>
-        </form>
+  const {registrarColaborador, crearEquipo} = props
+
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+    console.log("Manejar el Envio");
+    let datosAEnviar = {
+      nombre,
+      puesto,
+      foto,
+      equipo,
+    };
+    registrarColaborador(datosAEnviar);
+  };
+
+  const manejarNuevoEquipo = (e) => {
+    e.preventDefault()
+    crearEquipo({titulo, colorPrimario: color})
+  }
+
+  return (
+    <section className="formulario">
+      <form onSubmit={manejarEnvio}>
+        <h2>Rellena el formulario para crear el colaborador.</h2>
+        <Campo
+          titulo="Nombre"
+          placeholder="Ingresar nombre"
+          required
+          valor={nombre}
+          actualizarValor={actualizarNombre}
+        />
+        <Campo
+          titulo="Puesto"
+          placeholder="Ingresar puesto"
+          required
+          valor={puesto}
+          actualizarValor={actualizarPuesto}
+        />
+        <Campo
+          titulo="Foto"
+          placeholder="Ingresar enlace de foto"
+          required
+          valor={foto}
+          actualizarValor={actualizarFoto}
+        />
+        <ListaOpciones
+          valor={equipo}
+          actualizarEquipo={actualizarEquipo}
+          equipos={props.equipos}
+        />
+        {/* <Boton ="Crear" /> */}
+        <Boton>Crear</Boton>
+      </form>
+      <form onSubmit={manejarNuevoEquipo}>
+        <h2>Rellena el formulario para crear el equipo.</h2>
+        <Campo
+          titulo="Titulo"
+          placeholder="Ingresar titulo"
+          required
+          valor={titulo}
+          actualizarValor={actualizarTitulo}
+        />
+        <Campo
+          titulo="Color"
+          placeholder="Ingresar el color en Hex"
+          required
+          valor={color}
+          actualizarValor={actualizarColor}
+          type="color"
+        />
+        <Boton>Registrar equipo</Boton>
+      </form>
     </section>
-}
+  );
+};
 
 export default Formulario;
